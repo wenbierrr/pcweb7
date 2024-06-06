e0d2a3a2a46e758cd47d360a4af7a3d05723e9ae
wenbierrr <h.wenbin11@gmail.com>
2024-06-06 12:11:51 +0800

    minor edits

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { collection, getDocs } from "firebase/firestore";
